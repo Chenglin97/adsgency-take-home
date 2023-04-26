4bc2cb23bf0fa6e3af9a3d1121bc0d15051d656f
Chenglin Wei <chengliw@andrew.cmu.edu>
2023-04-26 02:25:01 -0700

    Add redirect_uri env

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const http = require("http");
 const server = http.createServer(app);
-
+require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
@@ -13,12 +13,15 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-const csrfState = Math.random().toString(36).substring(2);
-
-const port = 3001 || process.env.port;
+const port = process.env.port || 3000;
 server.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
+const SERVER_ENDPOINT_REDIRECT =
+  process.env.SERVER_ENDPOINT_REDIRECT || "localhost:3000";
+const redirect = encodeURIComponent(SERVER_ENDPOINT_REDIRECT);
+
+console.log(redirect);
 
 const CLIENT_KEY = "awujnpxn69c76n3p";
 app.get("/oauth", (req, res) => {
@@ -28,9 +31,9 @@ app.get("/oauth", (req, res) => {
   let url = "https://www.tiktok.com/auth/authorize/";
 
   url += `?client_key=${CLIENT_KEY}`;
-  url += "&scope=user.info.basic,video.list";
+  url += "&scope=user.info.basic";
   url += "&response_type=code";
-  url += "&redirect_uri={SERVER_ENDPOINT_REDIRECT}";
+  url += `&redirect_uri=${redirect}`;
   url += "&state=" + csrfState;
 
   res.redirect(url);
