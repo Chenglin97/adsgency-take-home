fa01b1ef62259d70ced29a07c0310d2d80a4fc48
Chenglin Wei <chengliw@andrew.cmu.edu>
2023-05-02 16:14:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -30,22 +30,50 @@ const CALLBACK_URL =
   process.env.CALLBACK_URL ||
   "https://adsgency-take-home.onrender.com/authCallback";
 
-const CLIENT_KEY = "awbpei30wwvrdl6a";
 app.get("/oauth", (req, res) => {
+  const CLIENT_KEY = "awbpei30wwvrdl6a";
+  const DOMAIN = "adsgency-take-home.onrender.com";
   const csrfState = Math.random().toString(36).substring(2);
   res.cookie("csrfState", csrfState, { maxAge: 60000 });
+  const redirect = encodeURIComponent(`https://${DOMAIN}/tiktok`);
 
   let url = "https://www.tiktok.com/auth/authorize/";
 
-  url += `?client_key=${CLIENT_KEY}`;
+  url += "?client_key=" + CLIENT_KEY;
   url += "&scope=user.info.basic";
   url += "&response_type=code";
-  url += `&redirect_uri=${encodeURIComponent(SERVER_ENDPOINT_REDIRECT)}`;
+  url += "&redirect_uri=" + redirect;
   url += "&state=" + csrfState;
 
   res.redirect(url);
+  // const csrfState = Math.random().toString(36).substring(2);
+  // res.cookie("csrfState", csrfState, { maxAge: 60000 });
+
+  // let url = "https://www.tiktok.com/auth/authorize/";
+
+  // url += `?client_key=${CLIENT_KEY}`;
+  // url += "&scope=user.info.basic";
+  // url += "&response_type=code";
+  // url += `&redirect_uri=${encodeURIComponent(SERVER_ENDPOINT_REDIRECT)}`;
+  // url += "&state=" + csrfState;
+
+  // res.redirect(url);
 });
 app.post("/authCallback", (req, res) => {
   console.log(req.body);
   res.send(200);
+});
+
+app.get("/tiktok", (req, res) => {
+  const { code, scopes, state } = req.query;
+
+  // Log the received information
+  console.log("Code:", code);
+  console.log("Scopes:", scopes);
+  console.log("State:", state);
+
+  // Do any further processing, such as verifying the state parameter, exchanging the code for an access token, or storing the received data
+
+  // Send a response to the client
+  res.status(200).send("Received TikTok authentication data");
 });
