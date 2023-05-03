b0224b95d9706a65db6fe58d2e62814ca5d4971c
Chenglin Wei <chengliw@andrew.cmu.edu>
2023-05-02 17:31:06 -0700

    Allow logout

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ const path = require("path");
 const http = require("http");
 const server = http.createServer(app);
 require("dotenv").config();
+const session = require("express-session");
+const axios = require("axios");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "b43bc7181680b12b83f67c99d9970a5f",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 }, // 1 hour
+  })
+);
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
@@ -24,11 +35,6 @@ const port = process.env.port || 3000;
 server.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
-const SERVER_ENDPOINT_REDIRECT =
-  process.env.SERVER_ENDPOINT_REDIRECT || "localhost:3000";
-const CALLBACK_URL =
-  process.env.CALLBACK_URL ||
-  "https://adsgency-take-home.onrender.com/authCallback";
 
 app.get("/oauth", (req, res) => {
   const CLIENT_KEY = "awbpei30wwvrdl6a";
@@ -46,34 +52,59 @@ app.get("/oauth", (req, res) => {
   url += "&state=" + csrfState;
 
   res.redirect(url);
-  // const csrfState = Math.random().toString(36).substring(2);
-  // res.cookie("csrfState", csrfState, { maxAge: 60000 });
-
-  // let url = "https://www.tiktok.com/auth/authorize/";
-
-  // url += `?client_key=${CLIENT_KEY}`;
-  // url += "&scope=user.info.basic";
-  // url += "&response_type=code";
-  // url += `&redirect_uri=${encodeURIComponent(SERVER_ENDPOINT_REDIRECT)}`;
-  // url += "&state=" + csrfState;
-
-  // res.redirect(url);
 });
 app.post("/authCallback", (req, res) => {
   console.log(req.body);
   res.send(200);
 });
 
-app.get("/tiktok", (req, res) => {
+app.get("/tiktok", async (req, res) => {
   const { code, scopes, state } = req.query;
 
-  // Log the received information
-  console.log("Code:", code);
-  console.log("Scopes:", scopes);
-  console.log("State:", state);
+  const CLIENT_KEY = "awbpei30wwvrdl6a";
+  const CLIENT_SECRET = "b43bc7181680b12b83f67c99d9970a5f";
+  const DOMAIN = "adsgency-take-home.onrender.com";
+  const redirect = encodeURIComponent(`https://${DOMAIN}/tiktok`);
 
-  // Do any further processing, such as verifying the state parameter, exchanging the code for an access token, or storing the received data
+  try {
+    const response = await axios.post(
+      "https://www.tiktok.com/oauth/access_token/",
+      {
+        client_key: CLIENT_KEY,
+        client_secret: CLIENT_SECRET,
+        code: code,
+        grant_type: "authorization_code",
+        redirect_uri: redirect,
+      }
+    );
 
-  // Send a response to the client
-  res.status(200).send("Received TikTok authentication data");
+    const { access_token, refresh_token } = response.data;
+
+    req.session.user = {
+      access_token,
+      refresh_token,
+      scopes,
+      state,
+    };
+
+    res.redirect("/?login=success");
+  } catch (error) {
+    console.error("Error fetching access token:", error);
+    res.redirect("/?login=error");
+  }
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send("Error logging out.");
+    }
+    res.clearCookie("connect.sid");
+    res.status(200).send("Logged out successfully.");
+  });
+});
+
+app.get("/checkLogin", (req, res) => {
+  const isLoggedIn = req.session && req.session.user;
+  res.json({ isLoggedIn });
 });
